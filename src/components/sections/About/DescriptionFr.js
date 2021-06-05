4900b43b6fb43c0e4829f28762cab7086424f52c
Fleur <fleur.dalle@hotmail.com>
2021-06-05 19:15:49 +0200

    change texts

--- src/components/sections/About/DescriptionFr.js
+++ src/components/sections/About/DescriptionFr.js
@@ -8,7 +8,7 @@ const DescriptionFr = () => {
             <br></br>
             <br></br>
             Avec, à la base, un diplôme en Psychologie, J'ai toujours été <DescriptionSpan>curieuse</DescriptionSpan> d'en apprendre plus sur la programmation informatique.
-            Et voilà comment fin novembre 2020, j'ai commencé à apprendre du Python. J'imagine que cela m'a plu car, depuis ce jour, je n'ai jamais arrêté. Me spécialisant dans la <DescriptionSpan>MERN stack</DescriptionSpan> (MongoDB, Express, React, Node.js).
+            Et voilà comment fin novembre 2020, j'ai commencé à apprendre Python. J'imagine que cela m'a plu car, depuis ce jour, je n'ai jamais arrêté. Me spécialisant dans la <DescriptionSpan>MERN stack</DescriptionSpan> (MongoDB, Express, React, Node.js).
             <br></br>
             <br></br>
             Cette nouvelle voie lie mon côté <DescriptionSpan>créatif</DescriptionSpan> et <DescriptionSpan>logique</DescriptionSpan>. Je pousse toujours plus loin mon apprentissage. Bref, en résumé, je m'<DescriptionSpan>amuse</DescriptionSpan> beaucoup.
