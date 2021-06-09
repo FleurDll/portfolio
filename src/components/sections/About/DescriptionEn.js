0fc07895f3d7760e4033aa4df65a6b41abd5eafe
Fleur <fleur.dalle@hotmail.com>
2021-06-09 20:42:54 +0200

    pdf

--- src/components/sections/About/DescriptionEn.js
+++ src/components/sections/About/DescriptionEn.js
@@ -7,8 +7,8 @@ const DescriptionEn = () => {
             I'm a 25 years old <DescriptionSpan>software developer</DescriptionSpan> from France, although I grew up between different countries. I'm currently preparing for my future destination: <DescriptionSpan>Canada</DescriptionSpan>.
             <br></br>
             <br></br>
-            Starting with a psychology diploma, I've always been curious to learn more about computer science.
-            And just like that, late Novembre 2020 I started learning Python. I guess I liked it because I never stopped since then, oops. Specializing myself in the <DescriptionSpan>MERN stack</DescriptionSpan> (MongoDB, Express, React, Node.js).
+            Starting with a diploma in psychology, I've always been curious to learn more about computer science.
+            And just like that, late Novembre 2020 I started learning Python. I guess I liked it because I never stopped since then! Specializing myself in the <DescriptionSpan>MERN stack</DescriptionSpan> (MongoDB, Express, React, Node.js).
             <br></br>
             <br></br>
             This new path allows me to join my <DescriptionSpan>logical</DescriptionSpan> and <DescriptionSpan>creative</DescriptionSpan> sides. I always push my learnings further and never give up. Well, in short, i'm having a lot of <DescriptionSpan>fun</DescriptionSpan>.
--- src/components/sections/About/DescriptionFr.js
+++ src/components/sections/About/DescriptionFr.js
@@ -4,11 +4,11 @@ import { AboutDescription, DescriptionSpan } from "./AboutElements";
 const DescriptionFr = () => {
     return (
         <AboutDescription>
-            J'ai 25ans et je suis française, bien que j'ai grandi dans différents pays. Je suis actuellement en train de préparer ma prochaine destination : le <DescriptionSpan>Canada</DescriptionSpan>.
+            J'ai 25ans et je suis française, bien qu'ayant grandi dans différents pays. Je suis actuellement en train de préparer ma prochaine destination : le <DescriptionSpan>Canada</DescriptionSpan>.
             <br></br>
             <br></br>
             Avec, à la base, un diplôme en Psychologie, j'ai toujours été <DescriptionSpan>curieuse</DescriptionSpan> d'en apprendre plus sur la programmation informatique.
-            Et voilà comment fin novembre 2020, j'ai commencé à apprendre Python. J'imagine que cela m'a plu car, depuis ce jour, je n'ai jamais arrêté, oups. Me spécialisant dans la <DescriptionSpan>MERN stack</DescriptionSpan> (MongoDB, Express, React, Node.js).
+            Et voilà comment fin novembre 2020, j'ai commencé à apprendre Python. J'imagine que cela m'a plu car, depuis ce jour, je n'ai jamais arrêté ! Me spécialisant dans la <DescriptionSpan>MERN stack</DescriptionSpan> (MongoDB, Express, React, Node.js).
             <br></br>
             <br></br>
             Cette nouvelle voie me permet de lier mes côtés <DescriptionSpan>logique</DescriptionSpan> et <DescriptionSpan>créatif</DescriptionSpan>. Je pousse toujours plus loin mon apprentissage. Bref, en résumé, je m'<DescriptionSpan>amuse</DescriptionSpan> beaucoup.
