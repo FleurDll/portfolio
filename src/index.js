74710091044f07301f0ec89377a450bf79f43b9e
Fleur <fleur.dalle@hotmail.com>
2021-12-20 20:41:33 -0600

    google analytics removed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GA4React from "ga-4-react";
+// import GA4React from "ga-4-react";
 import './index.css';
 import App from './App';
 
-const ga4react = new GA4React("G-FQMJ7FQ77X");
+// const ga4react = new GA4React("G-FQMJ7FQ77X");
+
 
 (async () => {
-    await ga4react.initialize();
+    // await ga4react.initialize();
 
     ReactDOM.render(
         <App />,
